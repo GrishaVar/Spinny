b30b6f1f06aa57e2780040135a5909763ca8b50d
Grisha Vartanyan <grisha@vartanyan.com>
2019-06-13 13:57:17 +0200

    split main into classes

--- main.py
+++ main.py
@@ -6,40 +6,14 @@ from math import sin, cos, pi
 from shapes import ShapeCombination, Cube, SquarePyramid
 from matrix import Matrix, Vector
 
-width = 1024
-height = 576
-c = Vector(width/2, height/2)
-pos = Vector(0,0,0)
-movement = {
-    'w': Vector(0,0,1),
-    'a': Vector(-1,0,0),
-    's': Vector(0,0,-1),
-    'd': Vector(1,0,0),
-    ' ': Vector(0,1,0),
-    'q': Vector(0,-1,0),
-    }
-
 angle = pi/32  # very small angle
 rotator = Matrix(  # y unchanged, x and z move along a circle
     [cos(angle), 0, -sin(angle)],
     [0, 1, 0],
     [sin(angle), 0, cos(angle)],
 )
-grower = Matrix(  # x,z unchanged, y gets a little smaller
-    [1, 0, 0],
-    [0, 0.95, 0],
-    [0, 0, 1],
-)
-# You can multiply these matrices to get both effects.
-both = rotator * grower
 
 # LA skript: matrix multiplication is associative!
-
-root = Tk()
-root.geometry('{}x{}'.format(width, height))
-
-canvas = Canvas(root)
-canvas.pack(fill=BOTH, expand=1)
 
 def draw_circle(v, r, canvas_name, color='black'):
     x,y = v.value
@@ -50,9 +24,8 @@ def draw_circle(v, r, canvas_name, color='black'):
     y1 = y + r
     return canvas_name.create_oval(x0, y0, x1, y1, fill=color)
 
-def projection(v):
-
-    v -= pos
+def projection(v, camera, centre):
+    v -= camera.pos
     z = v.value[2] # TODO: better way to do this?
     
     sx = 1/(z+5)  # shift z forwards, away from the view. Use reciprocal to scale down with increased z
@@ -60,44 +33,76 @@ def projection(v):
     sx *= 800  # zoom way in (original pyramid is tiny)
     sy *= -800  # tk has height wrong way around I think
     res = Matrix((sx,0,0), (0,sy,0)) * v  # different from wiki one because z is my depth
-    res += c  # move to the middle
+    res += centre  # move to the middle
     return res
 
-def myDraw(shape):
-    canvas.delete('all')
+class Camera():
+    KEY_BINDINGS = {
+        'w': Vector(0,0,1),
+        'a': Vector(-1,0,0),
+        's': Vector(0,0,-1),
+        'd': Vector(1,0,0),
+        ' ': Vector(0,1,0),
+        'q': Vector(0,-1,0),
+    }
+    def __init__(self, v=Vector(0,0,0), speed=0.05):
+        self.pos = v
+        self.speed = speed
     
-    converted_points = []
+    def move(self, event):
+        self.pos += self.speed*self.KEY_BINDINGS.get(event.char, Vector(0,0,0))
+
+
+class Window():
+    def __init__(self, root, canvas, shape, width, height):
+        self.root = root
+        self.canvas = canvas
+        self.shape = shape
+        self.width = width
+        self.height = height
+
+        self.root.geometry('{}x{}'.format(width, height))
+        self.canvas.pack(fill=BOTH, expand=1)
+        
+        self.centre = Vector(self.width/2, self.height/2)
+        self.refresh = 50
+        self.camera = Camera()
+
+        self.canvas.bind_all('<w>', self.camera.move)
+        self.canvas.bind_all('<a>', self.camera.move)
+        self.canvas.bind_all('<s>', self.camera.move)
+        self.canvas.bind_all('<d>', self.camera.move)
+        self.canvas.bind_all('<space>', self.camera.move)
+        self.canvas.bind_all('<q>', self.camera.move)
     
-    for v in shape.points:
-        converted = projection(v)
-        converted_points.append(converted)
+    def start(self):
+        self.draw()
+        self.root.mainloop()
 
-        draw_circle(converted, 2, canvas, 'red')
+    def draw(self):
+        self.canvas.delete('all')
+        
+        converted_points = []
+        
+        for v in self.shape.points:
+            converted = projection(v, self.camera, self.centre)
+            converted_points.append(converted)
 
-    for p1, p2 in shape.lines:
-        p1_vect = converted_points[p1]
-        p2_vect = converted_points[p2]
+            #draw_circle(converted, 2, canvas, 'red')
 
-        canvas.create_line(*p1_vect.value, *p2_vect.value)
+        for p1, p2 in self.shape.lines:
+            p1_vect = converted_points[p1]
+            p2_vect = converted_points[p2]
 
-    p = canvas.create_polygon(*converted_points[0].value, *converted_points[1].value, *converted_points[4].value)
-    canvas.itemconfigure(p, fill='#603')
+            self.canvas.create_line(*p1_vect.value, *p2_vect.value)
 
-    shape.transform(rotator)  # yo linear algebra works
+        p = self.canvas.create_polygon(*converted_points[0].value, *converted_points[1].value, *converted_points[2].value, *converted_points[3].value)
+        self.canvas.itemconfigure(p, fill='#603')
 
-    root.after(50, lambda: myDraw(shape))
+        self.shape.transform(rotator)  # yo linear algebra works
 
-def move(event):
-    global pos
-    pos = pos + 0.05*movement[event.char]
+        self.root.after(self.refresh, self.draw)
 
-#root.after(400, myDraw)
-canvas.bind_all('<w>', move)
-canvas.bind_all('<a>', move)
-canvas.bind_all('<s>', move)
-canvas.bind_all('<d>', move)
-canvas.bind_all('<space>', move)
-canvas.bind_all('<q>', move)
 
 myShape = ShapeCombination(
     Cube(Vector(0,0,0)),
@@ -110,5 +115,10 @@ myShape = ShapeCombination(
     shift=Vector(-.5,-1.4,-.5),
 )
 
-myDraw(myShape)
-root.mainloop()
+if __name__ == '__main__':
+    root = Tk()
+    canvas = Canvas(root)
+    
+    window = Window(root, canvas, myShape, 1024, 576)
+    window.start()
+
