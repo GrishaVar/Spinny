a10597c601c1cc817a8912954b3c4da58d84140d
Grisha Vartanyan <grisha@vartanyan.com>
2019-06-25 21:43:46 +0200

    Added use of faces, renamed Matrix and Vector to M and V. Changed window size and updated point radius.
    yes I know this is too much for one commit

--- main.py
+++ main.py
@@ -2,12 +2,13 @@
 
 from tkinter import Tk, Canvas, Frame, BOTH
 from math import sin, cos, pi
+from random import randint, choice
 
 from shapes import ShapeCombination, Cube, SquarePyramid
-from matrix import Matrix, Vector
+from matrix import Matrix as M, Vector as V
 
 angle = pi/32  # very small angle
-rotator = Matrix(  # y unchanged, x and z move along a circle
+rotator = M(  # y unchanged, x and z move along a circle
     [cos(angle), 0, -sin(angle)],
     [0, 1, 0],
     [sin(angle), 0, cos(angle)],
@@ -28,43 +29,43 @@ def projection(v, camera, centre):
     v -= camera.pos
     z = v.value[2] # TODO: better way to do this?
     
-    sx = 1/(z+5)  # shift z forwards, away from the view. Use reciprocal to scale down with increased z
-    sy = 1/(z+5)
+    sx = 1/z  # shift z forwards, away from the view. Use reciprocal to scale down with increased z
+    sy = 1/z
     sx *= 800  # zoom way in (original pyramid is tiny)
     sy *= -800  # tk has height wrong way around I think
-    res = Matrix((sx,0,0), (0,sy,0)) * v  # different from wiki one because z is my depth
+    res = M((sx,0,0), (0,sy,0)) * v  # Apply transform from Q3 to Q2
     res += centre  # move to the middle
     return res
 
 class Camera():
     KEY_BINDINGS = {
-        'w': Vector(0,0,1),
-        'a': Vector(-1,0,0),
-        's': Vector(0,0,-1),
-        'd': Vector(1,0,0),
-        ' ': Vector(0,1,0),
-        'q': Vector(0,-1,0),
+        'w': V(0,0,1),
+        'a': V(-1,0,0),
+        's': V(0,0,-1),
+        'd': V(1,0,0),
+        ' ': V(0,1,0),
+        'q': V(0,-1,0),
     }
-    def __init__(self, v=Vector(0,0,0), speed=0.05):
+    def __init__(self, v=V(0,0,-5), speed=0.05):
         self.pos = v
         self.speed = speed
     
     def move(self, event):
-        self.pos += self.speed*self.KEY_BINDINGS.get(event.char, Vector(0,0,0))
+        self.pos += self.speed*self.KEY_BINDINGS.get(event.char, V(0,0,0))
 
 
 class Window():
-    def __init__(self, root, canvas, shape, width, height):
+    def __init__(self, root, canvas, shape):#, width, height):
         self.root = root
         self.canvas = canvas
         self.shape = shape
-        self.width = width
-        self.height = height
+        self.width = 1200
+        self.height = 700
 
-        self.root.geometry('{}x{}'.format(width, height))
+        self.root.attributes('-zoomed', True)
         self.canvas.pack(fill=BOTH, expand=1)
         
-        self.centre = Vector(self.width/2, self.height/2)
+        self.centre = V(self.width/2, self.height/2)
         self.refresh = 50
         self.camera = Camera()
 
@@ -87,17 +88,17 @@ class Window():
         for v in self.shape.points:
             converted = projection(v, self.camera, self.centre)
             converted_points.append(converted)
+            draw_circle(converted, 3-v.value[2], canvas, 'red')
 
-            #draw_circle(converted, 2, canvas, 'red')
-
+        for f in self.shape.faces:
+            p = self.canvas.create_polygon( *(converted_points[x].value for x in f) )
+            #self.canvas.itemconfigure(p, fill='#'+''.join([choice('012356789abcdef') for x in range(6)]))
+            self.canvas.itemconfigure(p, fill='#603')
+            
         for p1, p2 in self.shape.lines:
             p1_vect = converted_points[p1]
             p2_vect = converted_points[p2]
-
             self.canvas.create_line(*p1_vect.value, *p2_vect.value)
-
-        p = self.canvas.create_polygon(*converted_points[0].value, *converted_points[1].value, *converted_points[2].value, *converted_points[3].value)
-        self.canvas.itemconfigure(p, fill='#603')
 
         self.shape.transform(rotator)  # yo linear algebra works
 
@@ -105,20 +106,20 @@ class Window():
 
 
 myShape = ShapeCombination(
-    Cube(Vector(0,0,0)),
-    Cube(Vector(0,0,1)),
-    Cube(Vector(0,0,-1)),
-    Cube(Vector(0,-1,-1)),
-    Cube(Vector(0,-1,1)),
-    SquarePyramid(Vector(0,1,1)),
-    SquarePyramid(Vector(0,1,-1)),
-    shift=Vector(-.5,-1.4,-.5),
+    Cube(V(0,0,0)),
+    Cube(V(0,0,1)),
+    Cube(V(0,0,-1)),
+    Cube(V(0,-1,-1)),
+    Cube(V(0,-1,1)),
+    SquarePyramid(V(0,1,1)),
+    SquarePyramid(V(0,1,-1)),
+    shift=V(-.5,-1.4,-.5),
 )
 
 if __name__ == '__main__':
     root = Tk()
     canvas = Canvas(root)
     
-    window = Window(root, canvas, myShape, 1024, 576)
+    window = Window(root, canvas, myShape)#, 1024, 576)
     window.start()
 
