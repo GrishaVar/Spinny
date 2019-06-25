3685ccbfef81d7c677a95d52b8b3e6e8a9f8fb56
Grisha Vartanyan <grisha@vartanyan.com>
2019-06-25 23:50:20 +0200

    added first attempt at rotation

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
 
 from tkinter import Tk, Canvas, Frame, BOTH
-from math import sin, cos, pi
+from math import sin, cos, pi, atan
 from random import randint, choice
 
 from shapes import ShapeCombination, Cube, SquarePyramid
 from matrix import Matrix as M, Vector as V
 
-angle = pi/32  # very small angle
-rotator = M(  # y unchanged, x and z move along a circle
-    [cos(angle), 0, -sin(angle)],
-    [0, 1, 0],
-    [sin(angle), 0, cos(angle)],
-)
+def y_rotator(angle):
+    return M(  # y unchanged, x and z move along a circle
+        [cos(angle), 0, -sin(angle)],
+        [0, 1, 0],
+        [sin(angle), 0, cos(angle)],
+    )
+def x_rotator(angle):
+    return M(  # x unchanged, y and z move along a circle
+        [1, 0, 0],
+        [0, cos(angle), -sin(angle)],
+        [0, sin(angle), cos(angle)],
+    )
+obj_rotator = y_rotator(pi/32)  # very small angle
 
 # LA skript: matrix multiplication is associative!
 
@@ -28,9 +35,12 @@ def draw_circle(v, r, canvas_name, color='black'):
 def projection(v, camera, centre):
     v -= camera.pos
     z = v.value[2] # TODO: better way to do this?
+    dist_to_point = sum([c**2 for c in v.value])**0.5
+    v = y_rotator(camera.y_rotation_angle) * v  # rotate around y
+    v = x_rotator(camera.x_rotation_angle) * v  # rotate around x
     
-    sx = 1/z  # shift z forwards, away from the view. Use reciprocal to scale down with increased z
-    sy = 1/z
+    sx = 1/dist_to_point  # more distance => point closer to middle (?)
+    sy = 1/dist_to_point
     sx *= 800  # zoom way in (original pyramid is tiny)
     sy *= -800  # tk has height wrong way around I think
     res = M((sx,0,0), (0,sy,0)) * v  # Apply transform from Q3 to Q2
@@ -45,13 +55,53 @@ class Camera():
         'd': V(1,0,0),
         ' ': V(0,1,0),
         'q': V(0,-1,0),
+        'i': -1,
+        'j': 1,
+        'k': 1,
+        'l': -1,
     }
-    def __init__(self, v=V(0,0,-5), speed=0.05):
+    def __init__(self, v=V(0,0,-10), speed=0.05, rot_speed=pi/64):
         self.pos = v
         self.speed = speed
+        self.rot_speed = rot_speed
+        self.direction = V(0,0,1)
     
+    @property
+    def x(self):
+        return self.direction.value[0]
+    
+    @property
+    def y(self):
+        return self.direction.value[1]
+    
+    @property
+    def z(self):
+        return self.direction.value[2]
+    
+    @property
+    def y_rotation_angle(self):
+        if not self.z:
+            return pi  # think of better solution
+        return atan(self.x/self.z)
+    
+    @property
+    def x_rotation_angle(self):
+        if not self.z:
+            return pi
+        return atan(self.y/self.z)
+
     def move(self, event):
-        self.pos += self.speed*self.KEY_BINDINGS.get(event.char, V(0,0,0))
+        self.pos += self.speed * self.KEY_BINDINGS.get(event.char, V(0,0,0))
+
+    def turn(self, event):
+        multiplier = self.KEY_BINDINGS.get(event.char, 1)
+        if event.char in ('j', 'l'):
+            rotator = y_rotator
+        elif event.char in ('i', 'k'):
+            rotator = x_rotator
+        else:
+            raise ValueError('wrong key?')
+        self.direction = rotator(multiplier*self.rot_speed) * self.direction
 
 
 class Window():
@@ -75,6 +125,11 @@ class Window():
         self.canvas.bind_all('<d>', self.camera.move)
         self.canvas.bind_all('<space>', self.camera.move)
         self.canvas.bind_all('<q>', self.camera.move)
+        
+        self.canvas.bind_all('<j>', self.camera.turn)
+        self.canvas.bind_all('<l>', self.camera.turn)
+        self.canvas.bind_all('<i>', self.camera.turn)
+        self.canvas.bind_all('<k>', self.camera.turn)
     
     def start(self):
         self.draw()
@@ -93,14 +148,14 @@ class Window():
         for f in self.shape.faces:
             p = self.canvas.create_polygon( *(converted_points[x].value for x in f) )
             #self.canvas.itemconfigure(p, fill='#'+''.join([choice('012356789abcdef') for x in range(6)]))
-            self.canvas.itemconfigure(p, fill='#603')
+            self.canvas.itemconfigure(p, fill='#603', stipple='gray50')
             
         for p1, p2 in self.shape.lines:
             p1_vect = converted_points[p1]
             p2_vect = converted_points[p2]
             self.canvas.create_line(*p1_vect.value, *p2_vect.value)
 
-        self.shape.transform(rotator)  # yo linear algebra works
+        self.shape.transform(obj_rotator)  # yo linear algebra works
 
         self.root.after(self.refresh, self.draw)
 
