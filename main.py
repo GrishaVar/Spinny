82c9496b59bcbd3d483f877309cd506b957968d1
Grisha Vartanyan <grisha@vartanyan.com>
2019-06-28 14:37:47 +0200

    Added info box

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+import time
 from tkinter import Tk, Canvas, Frame, BOTH, EventType
 from math import sin, cos, pi, atan
 from random import randint, choice
+from collections import OrderedDict
 
 from shapes import ShapeCombination, Cube, SquarePyramid
 from matrix import Matrix as M, Vector as V
@@ -50,6 +52,7 @@ def projection(v, camera, centre):
     res += centre  # move to the middle
     return res
 
+
 class Camera:
     def __init__(self, pos=V(0,-10,0), view=V(0,1,0), speed=0.1, rot_speed=pi/64):
         self.pos = pos
@@ -89,6 +92,56 @@ class Camera:
         self.view = rot_matrix * self.view
 
 
+class InfoBox:
+    def __init__(self, canvas, pos, width, fill='white', border='black'):
+        self.canvas = canvas
+        self.x, self.y = pos
+        self.width = width
+        
+        self.items = OrderedDict()
+        self.counter = 0
+        
+        self.text_box = self.canvas.create_polygon(
+            0, 0,
+            fill=fill,
+            outline=border,
+            width=2,   
+        )
+    
+    def add(self, item, default='', rounding=None):
+        if item in self.items:
+            return  # TODO: what should I do here?
+        obj = self.canvas.create_text(
+            self.x+2,
+            self.y+2 + 17*self.counter,
+            anchor="nw",
+            font='Arial 13'
+        )
+        self.items[item] = (default, obj, rounding)
+        self.counter += 1
+        self.resize_box()
+
+    def draw(self, *args):
+        if len(args) != len(self.items):
+            raise TypeError('Too many/few arguments')
+        self.canvas.tag_raise(self.text_box)
+        for item, value in zip(self.items, args):
+            default, obj, rounding = self.items[item]
+            if rounding is not None:
+                value = round(value, rounding)
+            self.canvas.itemconfig(obj, text=default.format(value))
+            self.canvas.tag_raise(obj)
+
+    def resize_box(self):
+        self.canvas.coords(
+            self.text_box,
+            self.x, self.y,
+            self.x, self.y + 17*self.counter + 4,  # TODO: fix magic numbers
+            self.x+self.width, self.y + 17*self.counter + 4,
+            self.x+self.width, self.y
+        )
+
+
 class Window:
     KEY_BINDINGS = {
         'w': V(0,1,0),
@@ -112,17 +165,27 @@ class Window:
 
         self.w2 = self.width/2
         self.h2 = self.height/2
-        self.mouse = [0, 0]
-        self.paused = False
-        self.paused_text = canvas.create_text(self.w2, 10, anchor="n", font='Arial 50')
+
+        self.duration = 1
 
         self.root.attributes('-zoomed', True)
         self.root.config(cursor='none')
         self.canvas.pack(fill=BOTH, expand=1)
         
         self.centre = V(self.w2, self.h2)
-        self.refresh = 50
+        self.refresh = 30
         self.camera = Camera()
+        self.mouse = [0, 0]
+        self.paused = False
+        self.paused_text = self.canvas.create_text(self.w2, 10, anchor="n", font='Arial 50')
+        
+        self.infobox = InfoBox(self.canvas, (5,5), 100)
+        self.infobox.add('x', default='X = {}', rounding=2)
+        self.infobox.add('y', default='Y = {}', rounding=2)
+        self.infobox.add('z', default='Z = {}', rounding=2)
+        self.infobox.add('θx', default='θx = {}', rounding=2)
+        self.infobox.add('θz', default='θz = {}', rounding=2)
+        self.infobox.add('fps', default='FPS: {}', rounding=2)
 
         self.canvas.bind_all('<w>', self.move_input)
         self.canvas.bind_all('<a>', self.move_input)
@@ -141,13 +204,18 @@ class Window:
         self.canvas.bind_all('<Escape>', self.toggle_motion)
         self.canvas.bind_all('<Leave>', self.pause_motion)
         self.canvas.bind_all('<Control-r>', self.reset_camera)
-        self.canvas.bind_all('<Control-c>', self.quit)
+        self.canvas.bind_all('<Control-q>', self.quit)
+    
+    @property
+    def fps(self):
+        return 1000/(self.duration+self.refresh)
     
     def start(self):
         self.draw()
         self.root.mainloop()
 
     def draw(self):
+        t = time.time()
         self.canvas.delete('clearable')
 
         self.camera.turn(*self.mouse_to_angles())
@@ -172,6 +240,9 @@ class Window:
             self.canvas.create_line(*p1_vect.value, *p2_vect.value, tag='clearable')
 
         self.shape.transform(obj_rotator)  # yo linear algebra works
+
+        self.update_text()
+        self.duration = time.time() - t
 
         if not self.paused:
             self.root.after(self.refresh, self.draw)
@@ -201,11 +272,21 @@ class Window:
         y = -mx*pi/1000  # TODO invert y-axis with the negative sign?
         return x, y
     
-    def pause_motion(self, event):
+    def update_text(self):
+        self.infobox.draw(  # fill these lines automatically?
+            self.camera.pos.value[0],
+            self.camera.pos.value[1],
+            self.camera.pos.value[2],
+            self.camera.x_rotation_angle,
+            self.camera.z_rotation_angle,
+            self.fps,
+        )
+    
+    def pause_motion(self, *args):
         if not self.paused:
             self.toggle_motion()  # keep the pause code in one place
     
-    def toggle_motion(self, event):
+    def toggle_motion(self, *args):
         self.paused = not self.paused
         self.root.config(cursor=CURSOR_VIS[self.paused])
         self.canvas.itemconfig(self.paused_text, text='PAUSED')
@@ -216,7 +297,7 @@ class Window:
     def quit(self, *args):
         self.root.destroy()
 
-    def reset_camera(self, event):
+    def reset_camera(self, *args):
         self.camera = Camera()  # TODO add a way of resetting to non-standard camera?
 
 myShape = ShapeCombination(
