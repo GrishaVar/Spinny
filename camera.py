3c238d8cb1019543a3b00f2f843122925a185133
Grisha Vartanyan <grisha@vartanyan.com>
2019-07-24 23:54:38 +0200

    Fixed view vector bug and up/down movement now independent of direction

--- camera.py
+++ camera.py
@@ -1,7 +1,7 @@
-from math import pi, atan
+from math import pi
 
 from matrix import Matrix as M, Vector as V
-from common import M3
+from common import V3, M3
 
 
 def projection(v, camera, centre):
@@ -13,7 +13,7 @@ def projection(v, camera, centre):
     sy = 1/v.length
     sx *= 800  # zoom way in (original pyramid is tiny)
     sy *= -800  # tk has y pointing down
-    res = M((sx,0,0), (0,0,sy)) * v  # Apply transform from Q3 to Q2
+    res = M((sx, 0, 0), (0, 0, sy)) * v  # Apply transform from Q3 to Q2
     res += centre  # move to the middle
     return res
 
@@ -21,8 +21,8 @@ def projection(v, camera, centre):
 class Camera:
     def __init__(
         self,
-        pos=V(0,-10,0),
-        angles=(0,0),
+        pos=V(0, -10, 0),
+        angles=(0, 0),
         speed=0.1,
         rot_speed=pi/64,
     ):
@@ -30,41 +30,49 @@ class Camera:
         self.x_angle, self.z_angle = angles
         self.speed = speed
         self.rot_speed = rot_speed
-        
-        self.angle_changed = False
+
         self._rot_matrix = None
         self._view = None
 
+        self.rot_matrix_outdated = False
+        self.view_outdated = False
+
     @property
     def rot_matrix(self):
-        if self.angle_changed or self._rot_matrix is None:
-            x = M3.x_rot(self.x_angle)
-            z = M3.z_rot(self.z_angle)
-            self._rot_matrix = x*z
+        if self.rot_matrix_outdated or self._rot_matrix is None:
+            mz = M3.z_rot(self.z_angle)
+            mx = M3.x_rot(self.x_angle)
+            self._rot_matrix = mz * mx  # other way around doesn't work???
+            self.rot_matrix_outdated = False
         return self._rot_matrix
 
     @property
-    def view(self):
-        if self.angle_changed or self._view is None:
-            res = V(0,1,0)
-            self._view = self.rot_matrix * res
-            self.angle_changed = False
+    def view(self):  # returns camera viewing direction as a normalised vector
+        if self.view_outdated or self._view is None:
+            self._view = self.rot_matrix * V3.j  # j is the default orientation
+            self.view_outdated = False
         return self._view
 
     def move(self, v):
-        self.pos += self.rot_matrix * (v*self.speed)
+        z = v.project((V3.k,))
+        xy = v - z  # should be done in matrix but this is faster
+        delta = (self.rot_matrix * xy) + z  # up/down independent of view direction
+        self.pos += delta * self.speed
 
     def turn(self, rad_x, rad_z):
         limit = pi/2
         if rad_z:
             self.z_angle += rad_z
             self.z_angle = self.z_angle % (2*pi)
-            self.angle_changed = True
+            self.set_angle_update()
         if rad_x:
             self.x_angle += rad_x
             if self.x_angle > limit:
                 self.x_angle = limit
             elif self.x_angle < -limit:
                 self.x_angle = -limit
-            self.angle_changed = True
+            self.set_angle_update()
 
+    def set_angle_update(self):
+        self.view_outdated = True
+        self.rot_matrix_outdated = True
